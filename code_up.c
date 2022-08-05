422ebef5ec6e7484f013337e1a5e64458ef90fb8
JellyBin <102145465+Jellybin06@users.noreply.github.com>
2022-08-05 20:44:04 +0900

    Add files via upload

---
+++ code_up.c
@@ -0,0 +1,39 @@
+//code up 1106
+//code up 1038
+#include<stdio.h>
+
+//int main(void) {
+//    int min_a = 0x80000000;
+//    int max_b = 0x7fffffff;
+//
+//    printf("%d %d", min_a, max_b);
+//    return 0;
+//}
+
+
+
+//int main() {
+//
+//	int Atime = 0, Btime = 0;
+//	scanf("%d %d", &Atime, &Btime);
+//	if (Btime < 30 && Atime > 0) {
+//		Btime += 30;
+//		Atime -= 1;
+//		printf("%d %d", Atime, Btime);
+//	}
+//	else if (Btime >= 30 && Atime > 0) {
+//		Btime -= 30;
+//		printf("%d %d", Atime, Btime);
+//	}
+//	else if (Atime == 0 && Btime < 30) {
+//		Atime += 23;
+//		Btime += 30;
+//		printf("%d %d", Atime, Btime);
+//	}
+//	else if (Atime == 0 && Btime >= 30) {
+//		Btime -= 30;
+//		printf("%d %d", Atime, Btime);
+//	}
+//
+//	return 0;
+//}
